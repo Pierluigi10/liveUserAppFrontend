7c91626b4d2f6a4702d0f18592018966e23e8697
pierluigi <pierluigibaiano@gmail.com>
2021-11-15 15:51:01 +0100

    add backend url to useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+const backendUrl = 'http://localhost:3022';
+
 function App() {
   const [users, setUsers] = useState([]);
 
+  // useEffect(() => {
+  //   setUsers([
+  //     {
+  //       name: "nnn1",
+  //     },
+  //     {
+  //       name: "nnn2",
+  //     },
+  //   ]);
+  // }, []);
+
+
   useEffect(() => {
-    setUsers([
-      {
-        name: "nnn1",
-      },
-      {
-        name: "nnn2",
-      },
-    ]);
-  }, []);
+    (async () => {
+        const response = await fetch(backendUrl);
+        const data = await response.json();
+        setUsers(data);
+    })();
+}, []);
+
   return (
     <div className="App">
       <h1>User Management APP</h1>

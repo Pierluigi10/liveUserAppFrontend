8f9642ac6ba3973fd85c364ebfc1fa3cb6af5d64
pierluigi <pierluigibaiano@gmail.com>
2021-11-15 15:51:50 +0100

    display as cards with info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-const backendUrl = 'http://localhost:3022';
+const backendUrl = "http://localhost:3022";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -17,24 +17,36 @@ function App() {
   //   ]);
   // }, []);
 
-
   useEffect(() => {
     (async () => {
-        const response = await fetch(backendUrl);
-        const data = await response.json();
-        setUsers(data);
+      const response = await fetch(backendUrl);
+      const data = await response.json();
+      setUsers(data);
     })();
-}, []);
+  }, []);
 
   return (
     <div className="App">
       <h1>User Management APP</h1>
       <section className="users">
-        <ul>
-          {users.map((user, index) => {
-            return <li key={index}> {user.name}</li>;
-          })}
-        </ul>
+        {users.map((user, index) => {
+          return (
+            <div key={index} className="userCard">
+              <div className="row">
+                <div className="label">Full Name:</div>
+                <div className="data">{user.name}</div>
+              </div>
+              <div className="row">
+                <div className="label">User Name:</div>
+                <div className="data">{user.username}</div>
+              </div>
+              <div className="row">
+                <div className="label">E-Mail:</div>
+                <div className="data">{user.email}</div>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
